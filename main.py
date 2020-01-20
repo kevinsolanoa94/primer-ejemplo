2ac3877b09f40932519edbdc8fe03be33d75d8f1
Kevin <krsa1994@gmail.com>
2020-01-20 15:54:09 -0600

    new branch change

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main(msg):
-	// Another comment branch 01
-	// Adding a the second comment to this file.
- 	// Print a message
 	print(msg)
+
+def secondary():
+	print("new function")
 
 main("Hello People")
